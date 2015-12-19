a6bcf40374aa756c9b3595113f50319a37982535
romanylabeeb <romany.lab@gmail.com>
2015-12-20 00:49:14 +0200

    lesson 4

---
+++ app/src/main/java/com/example/profit3/sunshine/ForecastFragment.java
@@ -0,0 +1,175 @@
+package com.example.profit3.sunshine;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.profit3.sunshine.connection.AppConnection;
+import com.example.profit3.sunshine.parser.WeatherDataParser;
+
+import org.json.JSONException;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Profit3 on 21/11/2015.
+ */
+public  class ForecastFragment extends Fragment {
+
+    private ArrayAdapter<String> forecastAdapter;
+
+    public ForecastFragment() {
+    }
+
+    /***
+     * init function
+     * @param savedInstanceState
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Add this line in order for this fragment to handle menu events.
+        setHasOptionsMenu(true);
+    }
+    //    @Override
+    public void onStart(){
+        super.onStart();
+        this.updateWeather();
+    }
+    /**
+     * for adding the fragment menu to main menu
+     * @param menu
+     * @param inflater
+     */
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.forecastfragment, menu);
+    }
+
+    /***
+     * set Fake Data on the menu
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return
+     */
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        forecastAdapter =
+                new ArrayAdapter<String>(
+                        getActivity(),
+                        R.layout.list_item_forecast,
+                        R.id.list_item_forecast_textview,
+                        new ArrayList<String>());
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(forecastAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String  text = forecastAdapter.getItem(position);
+                int duration = Toast.LENGTH_SHORT;
+                Toast.makeText(getActivity(), text, duration).show();
+                // Executed in an Activity, so 'this' is the Context
+// The fileUrl is a string URL, such as "http://www.example.com/image.png"
+                Intent detailIntent = new Intent(getActivity(), DetailActivity.class);
+                detailIntent.putExtra(Intent.EXTRA_TEXT,text);
+                // downloadIntent.setData(Uri.parse(fileUrl));
+                startActivity(detailIntent);
+            }
+        });
+
+        return rootView;
+    }
+
+    /**
+     * for select refresh call the action
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //TODO
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_refresh) {
+
+            //steps call async class & excute
+            updateWeather();
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+    private  void updateWeather(){
+        //steps call async class & excute
+        FetchWeatherTask weatherTask = new FetchWeatherTask();
+        SharedPreferences prefs= PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String location=prefs.getString(getString( R.string.pref_location_key),    getString(R.string.pref_location_default));
+        String unitType=prefs.getString(getString( R.string.pref_temperature_units_key),    getString(R.string.pref_default_temperature_unit));
+        weatherTask.execute(location,unitType);
+    }
+
+
+
+    public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
+
+        private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
+        private AppConnection appConnection;
+        @Override
+        protected String[] doInBackground(String... params) {
+            if (params.length == 0) {
+                return null;
+            }
+            this.appConnection=new AppConnection();
+            //postalCODE "SMOUHA"=21615
+            //Reference http://www.nmisr.com/vb/showthread.php?t=545267
+            final String FORECAST_BASE_URL_ONE_WEEK=appConnection.buildRequestURLByParameters(params[0],"json",params[1],7);
+
+            String forecastJsonStr=   this.appConnection.getResponseByUrl(FORECAST_BASE_URL_ONE_WEEK);
+
+
+            System.out.println("response="+forecastJsonStr);
+            try {
+                return WeatherDataParser.getWeatherDataFromJson(forecastJsonStr,7);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+
+        }
+
+        /**
+         * add result to main activity
+         * @param results
+         */
+        @Override
+        protected void onPostExecute(String[] results) {
+            if(results!=null){
+                forecastAdapter.clear();
+
+                forecastAdapter.addAll(results);
+
+            }
+
+        }
+    }
+}
